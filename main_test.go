d0a45f1999c1c999441588081359cc27e3445c50
anurag-manchanda <anurag.manchanda@coinhako.com>
2022-08-27 18:32:57 +0800

    added test

--- main_test.go
+++ main_test.go
@@ -30,6 +30,10 @@ func Test_function_which_return_1(t *testing.T) {
 			name: "test2",
 			want: 1,
 		},
+		{
+			name: "test3",
+			want: 1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
